6c3929c0c4281372d555b29eed1477df54fe0ee3
Cooper Bouchard <cobouchard11@gmail.com>
2026-01-11 18:56:56 -0500

    Added GitHub workflow for building robot code

--- src/main/java/com/frc6324/robot2026/subsystems/drive/DriveIOCTRE.java
+++ src/main/java/com/frc6324/robot2026/subsystems/drive/DriveIOCTRE.java
@@ -2,8 +2,6 @@ package com.frc6324.robot2026.subsystems.drive;
 
 import static com.frc6324.robot2026.subsystems.drive.DrivetrainConstants.*;
 
-import org.littletonrobotics.junction.Logger;
-
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.StatusSignalCollection;
 import com.ctre.phoenix6.hardware.CANcoder;
@@ -11,102 +9,103 @@ import com.ctre.phoenix6.hardware.Pigeon2;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 import com.frc6324.robot2026.generated.TunerConstants;
-
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.wpilibj.RobotBase;
+import org.littletonrobotics.junction.Logger;
 
-public sealed class DriveIOCTRE extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> implements DriveIO
-        permits DriveIOSim {
-    private final StatusSignalCollection gyroscopeSignals;
-    private final StatusSignal<AngularVelocity> pitchVelocitySignal;
-    private final StatusSignal<AngularVelocity> rollVelocitySignal;
-    private final StatusSignal<AngularVelocity> yawVelocity;
-    private final StatusSignal<Angle> rollSignal;
-    private final StatusSignal<Angle> pitchSignal;
-    private final StatusSignal<LinearAcceleration> accelerationX;
-    private final StatusSignal<LinearAcceleration> accelerationY;
-    private final SwerveDriveState state;
+public sealed class DriveIOCTRE extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder>
+    implements DriveIO permits DriveIOSim {
+  private final StatusSignalCollection gyroscopeSignals;
+  private final StatusSignal<AngularVelocity> pitchVelocitySignal;
+  private final StatusSignal<AngularVelocity> rollVelocitySignal;
+  private final StatusSignal<AngularVelocity> yawVelocity;
+  private final StatusSignal<Angle> rollSignal;
+  private final StatusSignal<Angle> pitchSignal;
+  private final StatusSignal<LinearAcceleration> accelerationX;
+  private final StatusSignal<LinearAcceleration> accelerationY;
+  private final SwerveDriveState state;
 
-    public DriveIOCTRE() {
-        super(
-                TalonFX::new,
-                TalonFX::new,
-                CANcoder::new,
-                TunerConstants.DrivetrainConstants,
-                ODOMETRY_UPDATE_FREQUENCY,
-                ODOMETRY_STDDEVS,
-                DEFAULT_VISION_STDDEVS,
-                SwerveDrive.regulateModuleConstantsForSimulation(
-                        TunerConstants.FrontLeft,
-                        TunerConstants.FrontRight,
-                        TunerConstants.BackLeft,
-                        TunerConstants.BackRight));
+  public DriveIOCTRE() {
+    super(
+        TalonFX::new,
+        TalonFX::new,
+        CANcoder::new,
+        TunerConstants.DrivetrainConstants,
+        ODOMETRY_UPDATE_FREQUENCY,
+        ODOMETRY_STDDEVS,
+        DEFAULT_VISION_STDDEVS,
+        SwerveDrive.regulateModuleConstantsForSimulation(
+            TunerConstants.FrontLeft,
+            TunerConstants.FrontRight,
+            TunerConstants.BackLeft,
+            TunerConstants.BackRight));
 
-        // Reset the pose in simulation to somewhere known
-        if (RobotBase.isSimulation()) {
-            super.resetPose(STARTING_POSE);
-        }
-
-        // Get the state and pigeon of the drivetrain
-        state = getState();
-        Pigeon2 pigeon = getPigeon2();
-
-        // Store signals from the pigeon we care about
-        pitchVelocitySignal = pigeon.getAngularVelocityYWorld();
-        rollVelocitySignal = pigeon.getAngularVelocityXWorld();
-        yawVelocity = pigeon.getAngularVelocityZWorld();
-        rollSignal = pigeon.getRoll();
-        pitchSignal = pigeon.getPitch();
-        accelerationX = pigeon.getAccelerationX();
-        accelerationY = pigeon.getAccelerationY();
-
-        // Create the signal collection
-        gyroscopeSignals = new StatusSignalCollection(
-                pitchSignal, pitchVelocitySignal,
-                rollSignal, rollVelocitySignal,
-                accelerationX, accelerationY);
-
-        // Set signal update frequencies
-        yawVelocity.setUpdateFrequency(ODOMETRY_UPDATE_FREQUENCY);
-        gyroscopeSignals.setUpdateFrequencyForAll(100);
-
-        // Set the odometry thread's priority to very high
-        getOdometryThread().setThreadPriority(99);
+    // Reset the pose in simulation to somewhere known
+    if (RobotBase.isSimulation()) {
+      super.resetPose(STARTING_POSE);
     }
 
-    @Override
-    public void logModuleStates(SwerveDriveState state) {
-        // Stop if the module states or targets are null so we don't cause an NPE
-        if (state.ModuleStates == null || state.ModuleTargets == null) {
-            return;
-        }
+    // Get the state and pigeon of the drivetrain
+    state = getState();
+    Pigeon2 pigeon = getPigeon2();
 
-        for (int i = 0; i < 4; i++) {
-            // Get the current module and its name
-            var module = getModule(i);
-            var name = MODULE_NAMES[i];
+    // Store signals from the pigeon we care about
+    pitchVelocitySignal = pigeon.getAngularVelocityYWorld();
+    rollVelocitySignal = pigeon.getAngularVelocityXWorld();
+    yawVelocity = pigeon.getAngularVelocityZWorld();
+    rollSignal = pigeon.getRoll();
+    pitchSignal = pigeon.getPitch();
+    accelerationX = pigeon.getAccelerationX();
+    accelerationY = pigeon.getAccelerationY();
 
-            // Log steering information
-            Logger.recordOutput(
-                    "Drive/" + name + "/Absolute Encoder Angle",
-                    module.getEncoder().getAbsolutePosition().getValue());
-            Logger.recordOutput("Drive/" + name + "/Steering Angle", state.ModuleStates[i].angle);
-            Logger.recordOutput("Drive/" + name + "/Target Steering Angle", state.ModuleTargets[i].angle);
-            // Log drive velocity information
-            Logger.recordOutput(
-                    "Drive/" + name + "/Drive Velocity", state.ModuleStates[i].speedMetersPerSecond);
-            Logger.recordOutput(
-                    "Drive/" + name + "/Target Drive Velocity", state.ModuleTargets[i].speedMetersPerSecond);
-        }
+    // Create the signal collection
+    gyroscopeSignals =
+        new StatusSignalCollection(
+            pitchSignal, pitchVelocitySignal,
+            rollSignal, rollVelocitySignal,
+            accelerationX, accelerationY);
+
+    // Set signal update frequencies
+    yawVelocity.setUpdateFrequency(ODOMETRY_UPDATE_FREQUENCY);
+    gyroscopeSignals.setUpdateFrequencyForAll(100);
+
+    // Set the odometry thread's priority to very high
+    getOdometryThread().setThreadPriority(99);
+  }
+
+  @Override
+  public void logModuleStates(SwerveDriveState state) {
+    // Stop if the module states or targets are null so we don't cause an NPE
+    if (state.ModuleStates == null || state.ModuleTargets == null) {
+      return;
     }
 
-    @Override
+    for (int i = 0; i < 4; i++) {
+      // Get the current module and its name
+      var module = getModule(i);
+      var name = MODULE_NAMES[i];
+
+      // Log steering information
+      Logger.recordOutput(
+          "Drive/" + name + "/Absolute Encoder Angle",
+          module.getEncoder().getAbsolutePosition().getValue());
+      Logger.recordOutput("Drive/" + name + "/Steering Angle", state.ModuleStates[i].angle);
+      Logger.recordOutput("Drive/" + name + "/Target Steering Angle", state.ModuleTargets[i].angle);
+      // Log drive velocity information
+      Logger.recordOutput(
+          "Drive/" + name + "/Drive Velocity", state.ModuleStates[i].speedMetersPerSecond);
+      Logger.recordOutput(
+          "Drive/" + name + "/Target Drive Velocity", state.ModuleTargets[i].speedMetersPerSecond);
+    }
+  }
+
+  @Override
   public void updateInputs(DriveInputs inputs) {
     // Update the last known odometry
     DrivingUtils.updateOdometry(state.Pose, state.Speeds);
-    
+
     // Copy the recorded state into the inputs
     inputs.copyFromState(state);
 
--- src/main/java/com/frc6324/robot2026/subsystems/drive/DriveIOSim.java
+++ src/main/java/com/frc6324/robot2026/subsystems/drive/DriveIOSim.java
@@ -3,24 +3,25 @@ package com.frc6324.robot2026.subsystems.drive;
 import static com.frc6324.robot2026.subsystems.drive.DrivetrainConstants.ODOMETRY_PERIOD;
 
 import com.frc6324.lib.DeltaTimeCalculator;
-
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 
 public final class DriveIOSim extends DriveIOCTRE {
-    private final Notifier notifier;
-    private final DeltaTimeCalculator deltaCalculator = new DeltaTimeCalculator();
-    
-    // TODO: update to using maple-sim like the template once it gets updated
+  private final Notifier notifier;
+  private final DeltaTimeCalculator deltaCalculator = new DeltaTimeCalculator();
 
-    public DriveIOSim() {
-        notifier = new Notifier(() -> {
-            var delta = deltaCalculator.get();
+  // TODO: update to using maple-sim like the template once it gets updated
 
-            updateSimState(delta, RobotController.getBatteryVoltage());
-        });
+  public DriveIOSim() {
+    notifier =
+        new Notifier(
+            () -> {
+              var delta = deltaCalculator.get();
 
-        notifier.setName("Simulation Thread");
-        notifier.startPeriodic(ODOMETRY_PERIOD);
-    }
+              updateSimState(delta, RobotController.getBatteryVoltage());
+            });
+
+    notifier.setName("Simulation Thread");
+    notifier.startPeriodic(ODOMETRY_PERIOD);
+  }
 }
