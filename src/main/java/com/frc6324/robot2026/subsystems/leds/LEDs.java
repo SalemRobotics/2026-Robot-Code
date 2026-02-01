195388f4d99a3afb518de251ec02d727b01fead5
Cooper Bouchard <cobouchard11@gmail.com>
2026-02-01 14:53:39 -0500

    Switch to using the LEDPattern API
    Final LED patterns still yet TBD

--- src/main/java/com/frc6324/robot2026/subsystems/leds/LEDs.java
+++ src/main/java/com/frc6324/robot2026/subsystems/leds/LEDs.java
@@ -5,15 +5,13 @@ import static com.frc6324.robot2026.subsystems.leds.LEDsConstants.*;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import lombok.Setter;
-import org.littletonrobotics.junction.Logger;
 
 public final class LEDs extends SubsystemBase {
   private final AddressableLED leds = new AddressableLED(LED_RIO_PWM_PORT);
   private final AddressableLEDBuffer buffer = new AddressableLEDBuffer(LED_BUFFER_LENGTH);
-  private Color currentColor = LED_DEFAULT_COLOR;
   @Setter private LEDState currentState = LEDState.INACTIVE;
 
   public LEDs() {
@@ -24,25 +22,13 @@ public final class LEDs extends SubsystemBase {
 
   @Override
   public void periodic() {
-    switch (currentState) {
-      case INACTIVE -> {
-        if (DriverStation.isEStopped()) {
-          solid(LED_ESTOP_COLOR);
-        } else {
-          solid(LED_DEFAULT_COLOR);
-        }
-      }
+    if (DriverStation.isEStopped()) {
+      LED_ESTOP_PATTERN.applyTo(buffer);
+    } else if (RobotController.isBrownedOut()) {
+      LED_BROWNOUT_PATTERN.applyTo(buffer);
+    } else switch (currentState) {
+      case INACTIVE -> LED_DEFAULT_PATTERN.applyTo(buffer);
     }
-
-    Logger.recordOutput("LEDs/CurrentColor", currentColor.toHexString());
-  }
-
-  private void solid(Color color) {
-    for (int i = 0; i < LED_BUFFER_LENGTH; i++) {
-      buffer.setLED(i, color);
-    }
-
-    currentColor = color;
   }
 
   /**
