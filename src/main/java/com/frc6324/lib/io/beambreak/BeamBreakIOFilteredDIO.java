09a60b08f3ac622a2721878da965a1d0d26cc04e
Cooper Bouchard <cobouchard11@gmail.com>
2026-02-05 23:10:03 -0500

    Added better docs for beam break IO layers

--- src/main/java/com/frc6324/lib/io/beambreak/BeamBreakIOFilteredDIO.java
+++ src/main/java/com/frc6324/lib/io/beambreak/BeamBreakIOFilteredDIO.java
@@ -5,29 +5,75 @@ import static edu.wpi.first.units.Units.Seconds;
 import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.DigitalGlitchFilter;
 import edu.wpi.first.wpilibj.DigitalInput;
+import lombok.Getter;
 
-public final class BeamBreakIOFilteredDIO extends BeamBreakIODIO {
+/**
+ * An extension of {@link BeamBreakIODIO} that adds a glitch filter that can reduce noise directly in the RoboRIO's FPGA.
+ * 
+ * <p> Because of limitations in the RoboRIO's FPGA, only 3 glitch filters can be created for a whole robot. If this limit is exceeded, an exception is thrown.
+ */
+public class BeamBreakIOFilteredDIO extends BeamBreakIODIO {
+    /**
+     * The FPGA glitch filter being used.
+     */
+    @Getter
     private final DigitalGlitchFilter filter;
 
+    /**
+     * Creates a new beam break sensor using a real digital I/O (DIO) device.
+     * 
+     * <p>The caller of this function is responsible for setting the amount of time the sensor must report a value for before the FPGA propogates the new value.
+     * @param dioDevice The Digital I/O device to read.
+     * @param filter The filter to add this sensor to.
+     * @param inverted Whether the sensor is inverted.
+     */
+    public BeamBreakIOFilteredDIO(DigitalInput dioDevice, DigitalGlitchFilter filter, boolean inverted) {
+        super(dioDevice, inverted);
+
+        this.filter = filter;
+        filter.add(sensor);
+    }
+
+    /**
+     * Creates a new beam break sensor using a real digital I/O (DIO) device.
+     * @param dioDevice The Digital I/O device to read.
+     * @param steadyTime The minimum time a sensor needs to maintain a state before the FPGA propogates it to the robot code.
+     * @param inverted Whether the sensor is inverted.
+     */
     public BeamBreakIOFilteredDIO(DigitalInput dioDevice, Time steadyTime, boolean inverted) {
         super(dioDevice, inverted);
 
         filter = new DigitalGlitchFilter();
         filter.add(sensor);
 
-        final double periodSeconds = steadyTime.in(Seconds) / 1e9;
+        final double periodSeconds = steadyTime.in(Seconds) * 1e9;
         filter.setPeriodNanoSeconds((long) periodSeconds);
-
     }
 
+    /**
+     * Creates a new beam break sensor using a real digital I/O (DIO) device.
+     * @param dioDevice The Digital I/O device to read.
+     * @param steadyTime The minimum time a sensor needs to maintain a state before the FPGA propogates it to the robot code.
+     */
     public BeamBreakIOFilteredDIO(DigitalInput dioDevice, Time steadyTime) {
         this(dioDevice, steadyTime, false);
     }
-
+    
+    /**
+     * Creates a new beam break sensor using a real digital I/O (DIO) device.
+     * @param dioPort The DIO port the device is on. Note that if the device is disconnected, the behavior of this I/O layer is not guaranteed.
+     * @param steadyTime The minimum time a sensor needs to maintain a state before the FPGA propogates it to the robot code.
+     * @param inverted Whether the sensor on {@code dioPort} is inverted.
+     */
     public BeamBreakIOFilteredDIO(int dioPort, Time steadyTime, boolean inverted) {
         this(new DigitalInput(dioPort), steadyTime, inverted);
     }
 
+    /**
+     * Creates a new beam break sensor using a real digital I/O (DIO) device.
+     * @param dioPort The DIO port the device is on. Note that if the device is disconnected, the behavior of this I/O layer is not guaranteed.
+     * @param steadyTime The minimum time a sensor needs to maintain a state before the FPGA propogates it to the robot code.
+     */
     public BeamBreakIOFilteredDIO(int dioPort, Time steadyTime) {
         this(dioPort, steadyTime, false);
     }
--- src/main/java/com/frc6324/lib/io/beambreak/BeamBreakIOSupplier.java
+++ src/main/java/com/frc6324/lib/io/beambreak/BeamBreakIOSupplier.java
@@ -1,20 +1,43 @@
 package com.frc6324.lib.io.beambreak;
 
 import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
+/**
+ * A beam break sensor that uses a Java function to determine the value reported by the "sensor" or simulation implementation.
+ */
 public final class BeamBreakIOSupplier implements BeamBreakIO {
     private final BooleanSupplier supplier;
 
+    /**
+     * Creates a new implementation of a "beam break" sensor given a function that returns a boolean.
+     * 
+     * <p>This implementation is meant to be used during non-replay simulation.
+     * @param supplier The function to call periodically that returns if the sensor is broken.
+     */
     public BeamBreakIOSupplier(BooleanSupplier supplier) {
         this.supplier = supplier;
+    }
+
+    /**
+     * Creates a new implementation of a "beam break" sensor given a function that returns a boolean.
+     * 
+     * <p>This implementation is meant to be used during non-replay simulation.
+     * @param supplier The function to call periodically that returns if the sensor is broken.
+     */
+    public BeamBreakIOSupplier(Supplier<Boolean> supplier) {
+        // Simply wrap the supplier to get the underlying primitive
+        this(() -> supplier.get().booleanValue());
     }
 
     @Override
     public void updateInputs(BeamBreakInputs inputs) {
         try {
+            // Try to call the function
             inputs.connected = true;
             inputs.broken = supplier.getAsBoolean();
         } catch (Exception e) {
+            // If the function throws an exception, propogate the exception and mark the sensor as disconnected.
             inputs.connected = false;
             throw e;
         }
